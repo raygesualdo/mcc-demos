4e0cb3e75118769968aedb13d5ed6bcce6bbb8e4
Ray Gesualdo <ray@rjgesualdo.com>
2017-01-15 12:46:53 -0500

    more demo prep

--- todoapp-styled-components/src/TodoItem.js
+++ todoapp-styled-components/src/TodoItem.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 const TodoItem = (props) => (
   <li className={`todo-item ${props.todo.completed && 'completed'}`}>
-    <div className="view">
-      <input
-        className="toggle"
-        type="checkbox"
-        checked={props.todo.completed}
-        onChange={props.onToggle}
-      />
-      <label className="todo-label">{props.todo.text}</label>
-      <button className="destroy" onClick={props.onDestroy} />
-    </div>
+    <input
+      className="toggle"
+      type="checkbox"
+      checked={props.todo.completed}
+      onChange={props.onToggle}
+    />
+    <label className="todo-label">{props.todo.text}</label>
+    <button className="destroy" onClick={props.onDestroy} />
   </li>
 )
 
